f9ca41c99dd90c6b48484e12ab2e38f535c7c141
EgorShva5 <shvarev.egor.a@yandex.ru>
2026-05-25 02:53:07 +0500

    19-21_1

--- final_tasks/Task19-21.py
+++ final_tasks/Task19-21.py
@@ -1,3 +1,23 @@
+#	№ 28704 (Уровень: Базовый)
+
+def f(s1,s2,n):
+    if s1+s2 >= 516: return n%2==0
+
+
+#№ 29351 Открытый вариант 2026 (Уровень: Базовый)
+'''def f(s1,s2,n):
+    if s1+s2 >= 154: return n%2==0
+    if n==0: return 0
+    
+    table = [f(s1+4,s2,n-1),f(s1,s2+4,n-1),f(s1*3,s2,n-1),f(s1,s2*3,n-1)]
+    
+    #19 задание - return any(table)
+    return all(table) if n%2==0 else any(table)
+
+print('19', [i for i in range(1,143) if f(11,i,2)])
+print('20', [i for i in range(1,143) if not f(11,i,1) and f(11,i,3)])
+print('21', [i for i in range(1,143) if not f(11,i,2) and f(11,i,4)])'''
+
 '''
 def f(s1,s2,n):
     if s1+s2 >= 211: return n % 2 == 0
